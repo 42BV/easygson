e9edb7a69dea4858df8368ce97f23edcdd40326b
robert-bor <bor.robert@gmail.com>
2013-12-01 17:31:18 +0100

    Issue #3 removed redundant constructor from WrappedElementException

--- src/main/java/org/easygson/WrappedElementException.java
+++ src/main/java/org/easygson/WrappedElementException.java
@@ -1,13 +1,6 @@
 package org.easygson;
 
 /**
- *
  * @author Robert Bor
  */
-public class WrappedElementException extends Exception {
-
-    public WrappedElementException(String message) {
-        super(message);
-    }
-
-}
+public class WrappedElementException extends Exception {}
